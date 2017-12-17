2a4dde1533e1d2eb2dc9bf15d8dda0ffb1aa347d
vduseev <vagiz@duseev.com>
2017-12-17 06:38:11 +0300

    Added PLN currency script

---
+++ accountant_db_sqlite/add_currencies.sql
@@ -0,0 +1,15 @@
+INSERT INTO currencies(
+  name,
+  code,
+  num,
+  digits_after_decimal,
+  warehoused_at,
+  updated_at
+) VALUES (
+  'Polish złoty',
+  'PLN',
+  '985',
+  2,
+  '2017-12-17T05:17:00+0500',
+  '2017-12-17T05:17:00+0500'
+);
